db3fe92834122a5f14d93e4a963ba759948ac315
M Becker <sucram.becker@gmx.de>
2019-08-18 22:15:38 +0200

    Added the Limit
    get_data() is now also returning the limit

--- getQuota.py
+++ getQuota.py
@@ -12,7 +12,7 @@ def get_data():
     vals = tables[0].values
 
     ip_num = vals[0, 0]
-    data_total = vals[0, 2]
+    data_max = vals[0, 11]
     data_in = vals[0, 3]
     data_out = vals[0, 4]
 
@@ -33,11 +33,19 @@ def get_data():
     else:
         data_out_num = float(data_out[:-3])
 
+    # Data Maximum
+    if data_max[len(data_max) - 2:] == 'GB':
+        data_max_num = float(data_max[:-3]) * 1000
+    elif data_max[len(data_out) - 2:] == 'KB':
+        data_max_num = float(data_max[:-3]) * 0.001
+    else:
+        data_max_num = float(data_max[:-3])
+
     # Data total
     data_total_num = data_out_num + data_in_num
 
     results = []
-    results.extend([data_in_num, data_out_num, data_total_num])
+    results.extend([data_in_num, data_out_num, data_total_num, data_max_num])
     return results
 
 
@@ -49,3 +57,4 @@ if __name__ == '__main__':
         print('In    (in MB): ', res[0])
         print('Out   (in MB): ', res[1])
         print('Total (in MB): ', res[2])
+        print('Limit (in MB): ', res[3])
